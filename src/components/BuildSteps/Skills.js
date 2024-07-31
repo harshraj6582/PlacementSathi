518762edcd3098485fa49bd22477b6cd0be04f2b
harshraj6582 <harshraj211430@gmail.com>
2024-07-31 16:15:15 +0530

    Bug Fixing

--- src/components/BuildSteps/Skills.js
+++ src/components/BuildSteps/Skills.js
@@ -51,7 +51,7 @@ const Skills = () => {
                         variant='solid'
                         colorScheme='purple'
                         m={0.5}
-                        key={skill.id}
+                        // key={skill.id}
                     >
                         <TagLabel>{skill.name}</TagLabel>
                         <TagCloseButton onClick={() => deleteSkill(skill.id)} />
--- src/components/ResumePreview.js
+++ src/components/ResumePreview.js
@@ -95,7 +95,7 @@ const ResumePreview = () => {
             spacing={1}
           >
             <VStack mx={2} alignItems={"flex-start"} w={"full"} spacing={6}>
-              // EDUCATION
+              {/* // EDUCATION */}
               <VStack alignItems={"flex-start"}>
                 <Heading as="h4" size="md" color={"gray.700"}>
                   EDUCATION
@@ -132,7 +132,7 @@ const ResumePreview = () => {
                   );
                 })}
               </VStack>
-              // EDUCATION // WORK EXPERIENCE
+              {/* // EDUCATION // WORK EXPERIENCE */}
               <VStack alignItems={"flex-start"}>
                 <Heading as="h4" size="md" color={"gray.700"}>
                   WORK EXPERIENCE
@@ -179,7 +179,7 @@ const ResumePreview = () => {
             </VStack>
 
             <VStack mx={2} alignItems={"flex-start"} w={"full"} spacing={6}>
-              // SKILLS
+              {/* // SKILLS */}
               <VStack alignItems={"flex-start"}>
                 <Heading as="h4" size="md" color={"gray.700"}>
                   SKILLS
